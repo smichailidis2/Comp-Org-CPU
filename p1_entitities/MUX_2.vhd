5de8bdab914a7a96d4504861e988ed4e82028c60
Michailidis Stergios <82328705+smichailidis2@users.noreply.github.com>
2023-04-10 21:12:30 +0300

    Add files via upload

---
+++ p1_entitities/MUX_2.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    18:00:11 04/10/2023 
+-- Design Name: 
+-- Module Name:    MUX2 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity MUX2 is
+    Port ( In0 : in  STD_LOGIC_VECTOR (31 downto 0);
+           In1 : in  STD_LOGIC_VECTOR (31 downto 0);
+           Mux_out : out  STD_LOGIC_VECTOR (31 downto 0);
+           SEL : in  STD_LOGIC);
+end MUX2;
+
+architecture Behavioral of MUX2 is
+
+begin
+
+with SEL select
+	Mux_out <= In0 when '0',
+				  In1 when '1';
+
+end Behavioral;
+
---
+++ p1_entitities/RF.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:43:05 04/10/2023 
+-- Design Name: 
+-- Module Name:    RF - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity RF is
+    Port ( Ard1 : in  STD_LOGIC_VECTOR (4 downto 0);
+           Ard2 : in  STD_LOGIC_VECTOR (4 downto 0);
+           Awr : in  STD_LOGIC_VECTOR (4 downto 0);
+           Dout1 : out  STD_LOGIC_VECTOR (31 downto 0);
+           Dout2 : out  STD_LOGIC_VECTOR (31 downto 0);
+           Din : in  STD_LOGIC_VECTOR (31 downto 0);
+           WrEn : in  STD_LOGIC;
+           clk : in  STD_LOGIC);
+end RF;
+
+architecture Behavioral of RF is
+
+begin
+
+
+end Behavioral;
+
